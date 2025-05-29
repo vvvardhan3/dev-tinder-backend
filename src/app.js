a1a02b2524d00e3a343e319717330f7c18fb477a
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2025-05-29 17:39:28 +0530

    ways of error handling

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const app = express();
 
+// one-way error handling
+app.use("/", (err, req, res, next) => {
+  res.status(500).send("An error occurred while retrieving the user data( method-2).")
+})
+
+// two-way error handling
 app.get("/getUserData", (req,res) => {
   try{
     throw new Error("User data not found");
     res.send("User data retrieved successfully");
   } catch (err){
-    res.status(500).send("An error occurred while retriving the user data.")
+    res.status(500).send("An error occurred while retrieving the user data.")
   }
 })
 
