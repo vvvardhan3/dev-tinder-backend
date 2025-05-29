296e43a4e20d4f2f1e608daaaca96f4a7d673413
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2025-05-29 17:41:15 +0530

    2nd method of error handling

--- src/app.js
+++ src/app.js
@@ -2,20 +2,18 @@ const express = require("express");
 
 const app = express();
 
-// one-way error handling
-app.use("/", (err, req, res, next) => {
-  res.status(500).send("An error occurred while retrieving the user data( method-2).")
-})
+// 1st-way error handling
+app.get("/getUserData", (req, res) => {
+  throw new Error("User data not found");
+  res.send("User data retrieved successfully");
+});
 
-// two-way error handling
-app.get("/getUserData", (req,res) => {
-  try{
-    throw new Error("User data not found");
-    res.send("User data retrieved successfully");
-  } catch (err){
-    res.status(500).send("An error occurred while retrieving the user data.")
-  }
-})
+// 2nd-way error handling
+app.use("/", (err, req, res, next) => {
+  res
+    .status(500)
+    .send("An error occurred while retrieving the user data( method-2).");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
