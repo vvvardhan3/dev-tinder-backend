1e78a8de502a8bddc05cf5a966f476c8d52c8be5
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2025-05-29 17:37:26 +0530

    One-way of error handling

--- src/app.js
+++ src/app.js
@@ -2,30 +2,14 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-// Admin
-app.use("/admin", adminAuth, (req, res, next) => {
-  console.log("Admin middleware executed");
-  next();
-});
-
-app.get("/admin/getallData", (req, res, next) => {
-  res.send("All data retrieved successfully");
-});
-
-app.get("/admin/deleteUser", (req, res, next) => {
-  res.send("User deleted successfully");
-});
-
-// User
-app.post("user/login", (req, res, next) => {
-  console.log("User login successful");
-});
-
-app.get("user/data", userAuth, (req, res, next) => {
-  res.send("User data retrieved successfully");
-});
+app.get("/getUserData", (req,res) => {
+  try{
+    throw new Error("User data not found");
+    res.send("User data retrieved successfully");
+  } catch (err){
+    res.status(500).send("An error occurred while retriving the user data.")
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
